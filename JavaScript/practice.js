6b1d42c11f033fa8e0f46d2248fc64199620cae1
Rashik Ghosh <rashikghosh2004@Rashiks-MacBook-Air.local>
2025-05-05 12:19:30 +0530

    week13 project for practice

---
+++ JavaScript/practice.js
@@ -0,0 +1,5 @@
+// console.log("2"+2+"2");
+// console.log("2"+2-"2");
+// console.log("Rajiv"+"100");
+// console.log("100"+20);
+
--- JavaScript/tempCodeRunnerFile.js
+++ JavaScript/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-let num = 0;
-// while (num >= 0) {
-//     num = parseInt(prompt("Enter a number:"));
-//     if (isNaN(num)) {
-//         console.log("Invalid input. Please enter a valid number.");
-//         continue; // Skip to the next iteration if input is invalid
-//     }
-//     console.log("Entered number: ", num);
-// }
+Rahul
